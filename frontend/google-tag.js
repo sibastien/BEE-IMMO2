b78a8a6d24250561f4000335d1046b981c64b4b6
Bee Consulting <beeconsulting@example.com>
2026-06-19 12:49:41 +0100

    Add Google Analytics tag

---
+++ frontend/google-tag.js
@@ -0,0 +1,21 @@
+// Google Analytics 4 tracking for public website pages.
+(function setupGoogleTag() {
+  const measurementId = 'G-Y7Q11BMF8D';
+
+  if (window.gtag) {
+    return;
+  }
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function gtag() {
+    window.dataLayer.push(arguments);
+  };
+
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+  document.head.appendChild(script);
+
+  window.gtag('js', new Date());
+  window.gtag('config', measurementId);
+})();
